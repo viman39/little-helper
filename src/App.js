283e0f3dfde3dd2986f9974c007655e61a3fcf3f
viman39 <vimanmanuel@gmail.com>
2023-06-05 18:08:31 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+  const inputRef = useRef();
+
+  const addQuestions = () => {
+    const questionsJSON = JSON.parse(inputRef.current.value);
+    const questionsParsed = questionsJSON.map(
+      ({ id, description, answers }) => ({
+        id,
+        description,
+        answers: answers.map(({ id, description }) => ({
+          id,
+          description,
+          isCorrect: null,
+        })),
+      })
+    );
+    console.log(questionsParsed);
+    setQuestions(questionsParsed);
+  };
+
+  const addAnswers = () => {
+    const answersJSON = JSON.parse(inputRef.current.value);
+    const qs = [...questions];
+
+    answersJSON.questionExams.forEach((question) => {
+      question.question.answers.forEach((answer) => {
+        const questionIndex = qs.findIndex(
+          (el) => el.id == question.question.id
+        );
+        const answerIndex = qs[questionIndex].answers.findIndex(
+          (el) => el.id == answer.id
+        );
+        console.log(
+          question.question.id,
+          answer.id,
+          questionIndex,
+          answerIndex
+        );
+        if (answerIndex != 0 && questionIndex != 0) {
+          qs[questionIndex].answers[answerIndex].isCorrect = answer.isCorrect;
+        }
+      });
+    });
+
+    setQuestions((oldQuestions) => qs);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {questions.length === 0 && (
+        <>
+          <input type="text" ref={inputRef}></input>
+          <button onClick={addQuestions}>QUESTIONS</button>
+        </>
+      )}
+      {questions.length !== 0 && (
+        <>
+          <div style={{ margin: "25px" }}>
+            <input type="text" ref={inputRef}></input>
+            <button onClick={addAnswers}>ANSWERS</button>
+          </div>
+
+          <div style={{ margin: "25px" }}>
+            {questions.map(({ id, description, answers }) => (
+              <div key={id} style={{ marginTop: "13px" }}>
+                <div>{description}</div>
+                {answers.map(({ id, description, isCorrect }) => (
+                  <div
+                    key={id}
+                    style={{
+                      color:
+                        isCorrect != null
+                          ? isCorrect
+                            ? "green"
+                            : "red"
+                          : "grey",
+                    }}
+                  >
+                    {description}
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Fixed from "./Fixed";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Fixed />
   </React.StrictMode>
 );
 
